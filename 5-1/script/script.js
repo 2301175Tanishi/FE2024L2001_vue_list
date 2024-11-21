a409dbc044ab15992ff55b0d9a5fc078645a306a
2301175Tanishi <2301175@s.asojuku.ac.jp>
2024-11-21 12:06:02 +0900

    success

--- 5-1/script/script.js
+++ 5-1/script/script.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#app',
     data() {
       return {
-        
+        fruits:['りんご','ぶどう','いちご','パイナップル']
       };
     }
   });
--- 5-2/script/script.js
+++ 5-2/script/script.js
@@ -2,7 +2,20 @@ new Vue({
   el: '#app',
   data() {
     return {
-
+      menus:[{
+        label:'TOP',
+        path:'./index.html'
+      },{
+        label:'ABOUT',
+        path:'./about.html'
+      },{
+        label:'SCHEDULE',
+        path:'./schedule.html'
+      },{
+        label:'CONTACT',
+        path:'./contact.html'
+      },
+    ]
     };
   }
 });
